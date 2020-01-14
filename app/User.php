a14ea33ee1f9a11415368c20184af087fb851774
Nawroten <50277945+Nawroten@users.noreply.github.com>
2020-01-14 10:10:00 +0100

    Update User.php

--- app/User.php
+++ app/User.php
@@ -37,4 +37,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    
+    public function Group(){
+        Return $thi->hasMany(Group::class);
+    }
 }
